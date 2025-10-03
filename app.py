5dc489e37d6bf590ac707f2e9d25bcc16d068ad1
aleksyzakupy-stack <aleksyzakupy@gmail.com>
2025-10-03 18:37:46 +0200

    Update app.py
    Focus 🧘‍♂️

--- app.py
+++ app.py
@@ -49,7 +49,18 @@ if st.session_state.running and st.session_state.current_index < len(st.session_
     if st.session_state.start_time is None:
         st.session_state.start_time = time.perf_counter()
 
-    answer = st.text_input("Wpisz wynik:", key=f"answer_{st.session_state.current_index}", autofocus=True)
+    answer = st.text_input("Wpisz wynik:", key=f"answer_{st.session_state.current_index}")
+    components.html(
+    f"""
+    <script>
+    var input = window.parent.document.querySelector('input[id="answer_{st.session_state.current_index}"]');
+    if(input) {{
+        input.focus();
+    }}
+    </script>
+    """,
+    height=0,
+)
 
     if answer:
         try:
